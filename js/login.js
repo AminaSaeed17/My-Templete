004202ac31b4e3c975b6afe80645d742a93a4993
unknown <aminasaeed2004@gmail.com>
2025-02-28 04:42:09 +0200

    Updated loin.js

--- js/login.js
+++ js/login.js
@@ -5,38 +5,38 @@ let alertInvalid = document.querySelector(".alert-invalid");
 let userList = [];
 
 if (localStorage.getItem("userList") != null) {
-    userList = JSON.parse(localStorage.getItem("userList"));
-    console.log(userList);
+  userList = JSON.parse(localStorage.getItem("userList"));
+  console.log(userList);
 }
 
 loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    login();
+  e.preventDefault();
+  login();
 });
 
 function login() {
-    users = {
-        emailLoin: emailLogin.value,
-        passLogin: passLogin.value,
-    };
-    if (isLoginValid(users) == true) {
-        console.log("true");
-        window.location.href = "../home.html";
-    } else {
-        alertInvalid.classList.remove("d-none");
-    }
+  users = {
+    emailLoin: emailLogin.value,
+    passLogin: passLogin.value,
+  };
+  if (isLoginValid(users) == true) {
+    console.log("true");
+    window.location.href = "./home.html";
+  } else {
+    alertInvalid.classList.remove("d-none");
+  }
 }
 
 function isLoginValid(users) {
-    for (let i = 0; i < userList.length; i++) {
-        // console.log(userList[i].userEmail);
-        if (
-            userList[i].userEmail.toLowerCase() === users.emailLoin.toLowerCase() &&
-            userList[i].userpass.toLowerCase() === users.passLogin.toLowerCase()
-        ) {
-            console.log("true");
-            localStorage.setItem("name", userList[i].userName);
-            return true;
-        }
+  for (let i = 0; i < userList.length; i++) {
+    // console.log(userList[i].userEmail);
+    if (
+      userList[i].userEmail.toLowerCase() === users.emailLoin.toLowerCase() &&
+      userList[i].userpass.toLowerCase() === users.passLogin.toLowerCase()
+    ) {
+      console.log("true");
+      localStorage.setItem("name", userList[i].userName);
+      return true;
     }
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 let formLogin = document.getElementById("formLogin");
 let signName = document.getElementById("sign-name");
 let signEmail = document.getElementById("sign-email");
@@ -11,92 +10,80 @@ let alertExist = document.querySelector(".alert-exist");
 let alertSucess = document.querySelector(".alert-sucess");
 let userList = [];
 
-
-
-
 if (localStorage.getItem("userList") !== null) {
-    userList = JSON.parse(localStorage.getItem("userList"));
+  userList = JSON.parse(localStorage.getItem("userList"));
 }
 
-
 formLogin.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (checkAllInputsValid() == true) {
-        addUsers();
-    }
+  e.preventDefault();
+  if (checkAllInputsValid() == true) {
+    addUsers();
+  }
 });
 
-
 function validationInputs(regex, element, alertMsg) {
-    let pattern = regex;
-    if (pattern.test(element.value)) {
-        alertMsg.classList.add("d-none");
-        return true;
-    } else {
-        alertMsg.classList.remove("d-none");
-        return false;
-    }
+  let pattern = regex;
+  if (pattern.test(element.value)) {
+    alertMsg.classList.add("d-none");
+    return true;
+  } else {
+    alertMsg.classList.remove("d-none");
+    return false;
+  }
 }
 
 function checkAllInputsValid() {
-    if (validationInputs(/^[A-Z][a-zA-Z' -]{1,49}$/, signName, alertName) == true && validationInputs(
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        signEmail,
-        alertEmail
-    ) == true && validationInputs(
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        signPass,
-        alertPass
-        ) == true) {
-        console.log("all Valid");
-        return true;
-    } else {
-        console.log("somthing is not valid");
-        return false;
-    }
+  if (
+    validationInputs(/^[A-Z][a-zA-Z' -]{1,49}$/, signName, alertName) == true &&
+    validationInputs(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      signEmail,
+      alertEmail
+    ) == true &&
+    validationInputs(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      signPass,
+      alertPass
+    ) == true
+  ) {
+    console.log("all Valid");
+    return true;
+  } else {
+    console.log("somthing is not valid");
+    return false;
+  }
 }
 
 function addUsers() {
-    users = {
-        userName: signName.value,
-        userEmail: signEmail.value,
-        userpass: signPass.value,
-    }
-    if (checkEmailExist(users) == true) {
-        alertExist.classList.remove("d-none");
-    } else {
-        alertExist.classList.add("d-none");
-        alertSucess.classList.remove("d-none");
-        userList.push(users);
-        console.log(users);
-        console.log(userList);
-        localStorage.setItem("userList", JSON.stringify(userList));
-        setTimeout(function () {
-            window.location.href = "./login.html";
-        },3000)
-    }
+  users = {
+    userName: signName.value,
+    userEmail: signEmail.value,
+    userpass: signPass.value,
+  };
+  if (checkEmailExist(users) == true) {
+    alertExist.classList.remove("d-none");
+  } else {
+    alertExist.classList.add("d-none");
+    alertSucess.classList.remove("d-none");
+    userList.push(users);
+    console.log(users);
+    console.log(userList);
+    localStorage.setItem("userList", JSON.stringify(userList));
+    setTimeout(function () {
+      window.location.href = "./login.html";
+    }, 3000);
+  }
 }
 
 function checkEmailExist(users) {
-    for (let i = 0; i < userList.length; i++){
-        if (userList[i].userEmail.toLowerCase() === users.userEmail.toLowerCase()){
-            return true;
-        }
+  for (let i = 0; i < userList.length; i++) {
+    if (userList[i].userEmail.toLowerCase() === users.userEmail.toLowerCase()) {
+      return true;
     }
+  }
 }
 // function clearInputs() {
 //     signName.value = null;
 //     signEmail.value = null;
 //     signPass.value = null;
 // }
-
-
-
-
-
-
-
-
-
-
-
